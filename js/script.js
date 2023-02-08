8701d6493916169bd8369b8573689dca013e8233
vbuarque <viniciusbuarque17@gmail.com>
2023-02-07 23:19:07 -0300

    FEAT: Finalizando a tela inicial :tada:

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Função para o menu responsivo */
 function menuShow() {
   var menuMobile = document.querySelector(".mobile-menu")
   if (menuMobile.classList.contains("open")) {
@@ -7,4 +8,30 @@ function menuShow() {
     menuMobile.classList.add("open")
     document.querySelector(".icon").name = "close-outline"
   }
+}
+
+/* Função para realizar o scroll down da página */
+function scrollDown() {
+  window.scrollTo( {
+    top: 1080,
+    behavior: "smooth"
+  })
+}
+
+/* Função para realiza o scroll up da página */
+const scrollUp = document.querySelector(".scroll-up");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 540) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
 }
