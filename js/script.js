00d186ccb3f0df0f9be34b32e1d8ce0c97e26169
vbuarque <viniciusbuarque17@gmail.com>
2023-02-12 19:00:07 -0300

    Feat: Adicionando o footer ao projeto e o slide carousel

--- js/script.js
+++ js/script.js
@@ -37,3 +37,14 @@ activeLink.forEach((link) => {
     link.classList.add("active")
   })
 })
+
+
+var slides = document.querySelectorAll(".slide")
+var currentSlide = 0
+var slideInterval = setInterval(nextSlide, 3000)
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active")
+  currentSlide = (currentSlide + 1) % slides.length
+  slides[currentSlide].classList.add("active")
+}
